520c217d291c6613dcf1437659646bda0dd66cb3
J2Alpha <johnjohnboeva@gmail.com>
2013-05-29 18:11:22 +0200

    added frontpage to doxygen, still need to fix doxygen syntax for some files

--- Plasmalib/main.cpp
+++ Plasmalib/main.cpp
@@ -4,6 +4,49 @@
  *  Created on: Apr 13, 2013
  *      Author: admin
  */
+/**
+@mainpage
+\section introduction
+plasmalib was born out of a need for a performant elemental particle trajectory simulator. I started writing this during my graduation at the TUDelft.
+None of the existing software I had found fit the bill, was hideously slow, or required me to learn yet another scripting language
+and all could not easily be extended or locked the data inside them. I also saw it as an opportunity to learn more of C++.
+the result is plasmalib: a single purpose charged particle trajectory simulator able to take:
+ 	 - a list of particles
+ 	 - some simple EM-field geometry
+ 	 - a time interval
+ 	 - and output acceleration, velocity and position over time.
+
+It is meant as a backend to a higher level language frontend like python, matlab or ruby
+
+\section dependencies
+the boost library, especically:
+ - boost/lexical_cast
+ - boost/multiprecision
+ - boost/algorithm
+
+\section compilation
+compilation is is done with a c++11 compatible compiler, make sure to set the appropriate flags to enable this.
+I compile the program via MinGW's g++ with eclipse juno as ide, I see no reason why this program would not compile on another compiler, but I'm no expert on this.
+
+\section running the program
+the program was designed to interoperate with external frontends like bluelight that automate the cmd line instruction and allow for easier reading of the output,
+ but can be ran stand-alone
+to invoke the program from the command line the following syntax is used
+
+	some/where/plasmalib(.exe) acceleratordata propellantdata targetdata
+
+	where:	acceleratordata: unicode file containing the limits and shape of the accelerator
+			propellantdata:  unicode file containing gasdata used as propellant
+			targetdata: is the path where the output should be stored
+
+the output of the program is a series of txt files, the txt files are named as the chemical element for which they have calculated the motions
+
+ 	 example: 	targetdata = h/e/re
+ 	 	 	 	propellantdata = somewhere/H_and_O.txt
+ 	 	 	 	acceleratordata = somewhere/else/limits.txt
+ 	 	 	 	cmd = some/where/plasmalib acceleratordata propellantdata targetdata
+ 	 	 	 	output = h/e/re/H.txt and h/e/re/O.txt
+**/
 
 #include <cstdio>
 #include <cstdlib>
